1d14fd95d981c83f56977048d668d3d7cbc58470
timmy3141 <hystuturo@gmail.com>
2026-02-10 15:34:13 +0100

    Affichage plateau

---
+++ main.py
@@ -0,0 +1,138 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title('2048')
+root.resizable(False, False)
+root.config(bg='#BBADA0')
+
+# Definition de la taille de la fenetre
+window_width = 660
+window_height = 810
+
+# Obtient la taille de l'écran
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Trouve le centre de l'écran
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+# Place la fenetre au centre de l'écran
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+# Definition des couleurs pour chaque valeur de case
+tiles_colors = {
+  "2": "#1B1D2F",
+  "4": "#283650",
+  "8": "#3C4C69",
+  "16": "#687792",
+  "32": "#8D99AE",
+  "64": "#CFC4D0",
+  "128": "#FA6D86",
+  "256": "#F43B5B",
+  "512": "#DF092E",
+  "1024": "#C00A2A",
+  "2048": "#7C0117",
+  "4096": "#51010F",
+  "8192": "#200006"
+}
+
+game = [
+    [2,4,8,16],
+    [32,64,128,256],
+    [512,1024,2048,4096],
+    [8192,0,0,0]
+]
+
+current_score = 0
+best_score = 0
+
+# -----------------------------------------HEADER-----------------------------------------
+
+top_frame = tk.Frame(root, bg="#BBADA0", height=150)
+top_frame.pack(fill="x", pady=(20, 0))
+
+# "Sous-frame" pour titre et scores
+top_row = tk.Frame(top_frame, bg="#BBADA0")
+top_row.pack(fill="x", padx=20)
+
+# "Sous-frame" pour bouton rejouer
+bottom_row = tk.Frame(top_frame, bg="#BBADA0")
+bottom_row.pack(fill="x", padx=20, pady=(10,0))
+
+# "Titre" 2048
+title_label = tk.Label(
+    top_row,
+    text="2048",
+    font=("Helvetica", 30, "bold"),
+    bg="#D9D9D9",
+    fg="#333",
+    width=8,
+    height=2,
+    bd=2,
+    relief="solid"
+)
+title_label.pack(side="left", padx=(0, 20))
+
+# Score actuel
+score = 0
+score_frame = tk.Frame(top_row, bg="#D9D9D9", bd=2, relief="solid")
+score_frame.pack(side="right", padx=(10,0))
+score_label = tk.Label(score_frame, text=f"Score\n{current_score}", font=("Helvetica", 16, "bold"), bg="#D9D9D9", fg="#333")
+score_label.pack(padx=10, pady=10)
+
+# Meilleur score
+best_frame = tk.Frame(top_row, bg="#D9D9D9", bd=2, relief="solid")
+best_frame.pack(side="right", padx=(10,0))
+best_label = tk.Label(best_frame, text=f"Best\n{best_score}", font=("Helvetica", 16, "bold"), bg="#D9D9D9", fg="#333")
+best_label.pack(padx=10, pady=10)
+
+# Bouton Rejouer
+restart_button = tk.Button(
+    bottom_row,
+    text="Rejouer",
+    font=("Helvetica", 14, "bold"),
+    bg="#D9D9D9",
+    fg="black",
+    width=12,
+    height=2
+)
+restart_button.pack(side="left")  # sous le "2048"
+
+
+#-----------------------------------------PLATEAU-----------------------------------------
+
+board_frame = tk.Frame(root, bg="#777676", width=600, height=600)
+board_frame.pack(pady=20)
+
+# Création du plateau
+tile_labels = [[None]*4 for _ in range(4)]
+for i in range(4):
+    for j in range(4):
+        label = tk.Label(
+            board_frame,
+            text="",
+            font=("Helvetica", 40, "bold"),
+            width=4, height=2,
+            bg="#FFFFFF",
+            fg="#FFFFFF",
+            bd=4,
+            relief="raised"
+        )
+        label.grid(row=i, column=j, padx=10, pady=10)
+        tile_labels[i][j] = label
+
+def display():
+    for i in range(4):
+        for j in range(4):
+            val = game[i][j]
+            if val == 0:
+                tile_labels[i][j].config(text="", bg="#FFFFFF")
+            else:
+                tile_labels[i][j].config(text=str(val), bg=tiles_colors[str(val)])
+
+
+
+display()
+root.mainloop()
